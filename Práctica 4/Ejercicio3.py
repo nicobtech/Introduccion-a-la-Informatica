6bfb19bf524c42bda0450092223aa940936badaa
FranAraujo1 <faraujoar@gmail.com>
2021-04-20 17:18:26 -0300

    Trabajos Practicos
    Ej 3

---
+++ Práctica 4/Ejercicio3.py
@@ -0,0 +1,6 @@
+archivo = open("/Users/frana/Downloads/UCEMA_Fundamentos_de_informatica-master/Python_intro/manipulacion_archivos.txt", 'r')
+leer = archivo.read()
+palabras = leer.split()
+
+print("La cantidad de palabras en el texto es de: ", len(palabras))
+
