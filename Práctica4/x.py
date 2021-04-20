e488e7cded9436cd3ba73b15132f56eebce390ab
FranAraujo1 <faraujoar@gmail.com>
2021-04-20 13:18:16 -0300

    Limpieza
    Eliminación de archivos vacíos

--- Práctica4/x.py
+++
@@ -1,9 +0,0 @@
-
-
-ocurrencias = []
-with open("/Users/frana/Downloads/UCEMA_Fundamentos_de_informatica-master/Python_intro/manipulacion_archivos.txt","r") as lineas:
-    for linea in lineas:
-        if linea[0] != "P":
-            ocurrencias.append(linea)
-
-print(len(ocurrencias))
