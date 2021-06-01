c556984ec81153c3cdbdd1e467658d87e796d81c
FranAraujo1 <faraujoar@gmail.com>
2021-05-31 21:08:26 -0300

    commit
    commit

--- Práctica Ciencia de Datos/Práctica - Pandas/Ejercicios.py
+++ Práctica Ciencia de Datos/Práctica - Pandas/Ejercicios.py
@@ -76,3 +76,28 @@ datos_ejemplo = {"nombre": ["Agustina", "Diana", "Karen", "Julián", "Emilio", "
 labels = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
 df = pd.DataFrame(datos_ejemplo, index=labels)
 print(df[0:3])
+
+#Ejercicio 8
+#Realizá un programa que seleccione e impirma las columnas "nombre" y "puntaje" del DataFrama anterior.
+
+import pandas as pd
+datos_ejemplo = {"nombre": ["Agustina", "Diana", "Karen", "Julián", "Emilio", "Miguel", "Mateo", "Laura", "Jorge", "Lucas"], "puntaje": [12.5, 9, 16.5, 13, 9, 20, 14.5, 10, 8, 19], "intentos": [1, 3, 2, 3, 2, 3, 1, 1, 2, 1], "califica": [1, 0, 1, 0, 0, 1, 1, 0, 0, 1]}
+lista = list(datos_ejemplo.items())
+
+for clave,valor in lista[0:2]:
+    print(clave,valor)
+
+#Ejercicio 9
+#Escribí un programa que dado el DataFrame anterior imprima los nombres en mayúscula y 
+# la longitud de los mismos en una nueva tabla.
+
+#CORREGIR
+import pandas as pd
+datos_ejemplo = {"nombre": ["Agustina", "Diana", "Karen", "Julián", "Emilio", "Miguel", "Mateo", "Laura", "Jorge", "Lucas"], "puntaje": [12.5, 9, 16.5, 13, 9, 20, 14.5, 10, 8, 19], "intentos": [1, 3, 2, 3, 2, 3, 1, 1, 2, 1], "califica": [1, 0, 1, 0, 0, 1, 1, 0, 0, 1]}
+lista = list(datos_ejemplo.items())
+lista[0]
+
+for clave,valor in lista[0]:
+    valor = valor.upper()
+    print(clave,valor)
+
